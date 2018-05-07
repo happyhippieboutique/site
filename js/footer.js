668349bf439a8647eca6945780e9476d1d13ab63
Randall Sturgis <badgerbacon7@gmail.com>
2018-05-06 19:50:44 -0700

    add footer template

---
+++ js/footer.js
@@ -0,0 +1,81 @@
+(function() {
+    var footer = `
+    <div class="empty-space marg-lg-b65"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                <div class="footer-logo-part simple-text">
+                    <a href="#" class="footer-logo">
+                        <img src="img/footer.png" alt="">
+
+                    </a>
+                    <p>ENVY clothing began out of an obsession with funny graphic tees, and love of fashion.</p>
+                </div>
+                <div class="empty-space marg-lg-b35"></div>
+                <ul class="social">
+                    <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
+                </ul>
+                <div class="empty-space marg-lg-b40"></div>
+            </div>
+            <div class="col-lg-3 col-lg-offset-1 col-md-offset-0 col-md-4 col-sm-5 col-sm-offset-1">
+                <div class="whoWe_are_descr">
+                    <div class="simple-text">
+                        <h5 class="h5 light">Contact</h5>
+                    </div>
+                    <div class="empty-space marg-xs-b20 marg-lg-b40"></div>
+                    <ul>
+                        <li>
+                            <span>Made With </span><i class="fa fa-heart" style="font-size: 20px"></i> in San Francisco
+                        </li>
+                        <li>
+                            <span>E-mail: </span><a href="mailto:jennifer@sturigs.io">jennifer@sturgis.io</a></li>
+
+                        <li>
+                            <span>Phone</span>: <a href="555-555-5555"> (555) 555-5555</a>
+                        </li>
+                        <li>
+                            <span>Work time: </span>mon - sun / 09:00 - 20:00
+                        </li>
+                    </ul>
+                </div>
+                <div class="empty-space marg-lg-b40"></div>
+            </div>
+            <div class="col-lg-3 col-lg-offset-1 col-md-4 col-sm-12">
+                <div class="footer-links">
+                    <div class="simple-text">
+                        <h5 class="h5 light">Quick links</h5>
+                    </div>
+                    <div class="empty-space marg-xs-b20 marg-lg-b40"></div>
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-0 col-sm-4 col-sm-offset-2 col-xs-6">
+                            <ul>
+                                <li><a href="/" >Home</a></li>
+                                <li><a href="about.html">About us</a></li>
+                                <li><a href="team.html">Shop</a></li>
+                                <!--<li><a href="gallery1.html">Gallery</a></li>-->
+                            </ul>
+                        </div>
+                        <div class="col-md-6 col-sm-4 col-xs-6">
+                            <ul class="ul">
+                                <li><a href="#">Offers</a></li>
+                                <li><a href="#testimonials-anchor" class="anchor-link">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="empty-space marg-lg-b40"></div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="copyright">
+            © 2018 All Rights reserved. Copyright Sturgis.io
+        </div>
+    </div>
+    <div class="empty-space marg-lg-b35"></div>
+        `
+    document.getElementById('footer').innerHTML = footer;
+})();
