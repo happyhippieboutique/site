0f5c0fb82228469afdbfcd7873e692be4cb25885
Randall Sturgis <badgerbacon7@gmail.com>
2018-05-09 21:23:51 -0700

    create session object shopify

--- js/shopify.js
+++ js/shopify.js
@@ -1,69 +1,127 @@
+(function(window) {
 
 
+    function Shopify(key) {
+        this.store = ShopifyBuy.buildClient(key);
+        this.url = new URL(window.location.href);
+        this.href = window.location.href;
+    }
 
-const client = ShopifyBuy.buildClient({
-  domain: 'ashtonblooclothing.myshopify.com',
-  storefrontAccessToken: '54698c4c37d13cfe1ffed8f3d7943294'
-});
+    Shopify.prototype.getProductId = function() {
+        return window.btoa("gid://shopify/Product/" + this.url.searchParams.get("product"));
+    }
 
-var url = new URL(window.location.href)
+    Shopify.prototype.getCollectionId = function() {
+        return window.btoa("gid://shopify/Collection/" + this.url.searchParams.get("collection"))
+    }
+
+    Shopify.prototype.getVariant = function(products, options) {
+        
+    }
+
+    shopify.store.collection.fetchWithProducts(collectionId).then((collection) => {
+        printProducts(collection.products);
+    });
+
+    function variantLookup(v) {
+        var match = true;
+        options.forEach(function(o) {
+            match = match && v.selectedOptions.find(so => so.name === o.name).value === o.selected; 
+        });
+        return match;
+    }
 
 
+    
+    var key = {
+        domain: 'ashtonblooclothing.myshopify.com',
+        storefrontAccessToken: '54698c4c37d13cfe1ffed8f3d7943294'
+    };
+
+    window.shopify = new Shopify(key);
+})(window);
+
+console.log(shopify);
 
 // Fetch a single product by ID
 
-if (window.location.href.indexOf('product-detail.html') != -1) {
-    const productId = window.btoa("gid://shopify/Product/" + url.searchParams.get("product")); //625662492731
-    client.product.fetch(productId).then((product) => {
+if (shopify.href.indexOf('product-detail.html') != -1) {
+    shopify.store.product.fetch(shopify.getProductId()).then((product) => {
+        console.log(product);
         
         document.getElementById("product-title").innerHTML = product.title;
         document.getElementById("description-short").innerHTML = product.descriptionHtml;
         document.getElementById("product-picture-1").src = product.images[0].src;
         document.getElementById("product-picture-1-thumb").src = product.images[0].src;
 
-        var sizes = product.options.find(x => x.name === "Size")
-        if (typeof sizes != "undefined") {
-            var select = document.getElementById("product-size");
-            sizes.values.forEach(function(e) {
-                var size = document.createElement("option");
-                size.text = e.value;
-                size.value = e.value;
-                select.add(size);
+        /*
+        p.variants = [
+            {
+                title: "XS / 2018",
+                price: "28.00",
+                selectedOptions: [
+                    { name: "Size", value: "XS"},
+                    { name: "Year", value: "2018"},
+                ]
+            },
+        ];
+        */
+
+        var options = [];
+        product.variants[0].selectedOptions.forEach(function (so) {
+            var values = product.options.find(o => o.name === so.name).values;
+            options.push({
+                name: so.name,
+                selected: so.value,
+                values: values,
             });
-            updateVariantInfo();
-            select.onchange = updateVariantInfo;
-        } // else do without variant
-    });
-}
+        });
 
-function updateVariantInfo() {
-    const productId = window.btoa("gid://shopify/Product/" + url.searchParams.get("product"));
-    client.product.fetch(productId).then((product) => {
-        var sizes = product.options.find(x => x.name === "Size")
-        if (typeof sizes != "undefined") {
-            // TODO: Support multiple options with options object
-            var selectedSize = document.getElementById("product-size").value;
-            var variant = product.variants.find(x => (x.selectedOptions[0].name === "Size" && x.selectedOptions[0].value === selectedSize));
-            if (typeof variant != "undefined") {
+        options.forEach(function(o) {
+            optionsSetup(o);
+        })
 
-                // set available if variant.available = true product-isavailable
-                if (variant.available) {
-                    document.getElementById("product-isavailable").innerHTML = "Yes";
-                }
-                
-                // set price to variant.price product-price
-                document.getElementById("product-price").innerHTML = variant.price;
-            }
+        function variantLookup(v) {
+            var match = true;
+            options.forEach(function(o) {
+                match = match && v.selectedOptions.find(so => so.name === o.name).value === o.selected; 
+            });
+            return match;
         }
+
+        if (product.variants.filter(variantLookup)[0].available) {
+            document.getElementById("product-isavailable").innerHTML = "Yes";
+        }
+        document.getElementById("product-price").innerHTML = product.variants.filter(variantLookup)[0].price;
+
     });
+}
+
+function optionsSetup(option) {
+    if (typeof option != "undefined") {
+        var selectContainer = document.getElementById("product-container-" + option.name.toLowerCase()).hidden = false;
+        var select = document.getElementById("product-" + option.name.toLowerCase());
+        option.values.forEach(function(v) {
+            var e = document.createElement("option");
+            e.text = v.value;
+            e.value = v.value;
+            select.add(e);
+        });
+        select.value = option.default;
+        select.onchange = optionsUpdate;
+    }
+}
+
+function optionsUpdate(e) {
+    console.log(e.target.id);
 }
 
 
 
-if (window.location.href.indexOf('products.html') != -1) {
+if (shopify.href.indexOf('products.html') != -1) {
     document.getElementsByClassName('btnGallery is-checked')[0].classList.remove('is-checked');
-    if (url.searchParams.get("collection")) {
-        var id = "collection" + url.searchParams.get("collection");
+    if (shopify.url.searchParams.get("collection")) {
+        var id = "collection" + shopify.url.searchParams.get("collection");
     } else {
         var id = "collectionAll";
     }
@@ -72,15 +130,15 @@ if (window.location.href.indexOf('products.html') != -1) {
     // list all collections
     
     // if "collection" in url
-    if (url.searchParams.get("collection")) {
-        const collectionId = window.btoa("gid://shopify/Collection/" + url.searchParams.get("collection")); //36392834523
-        client.collection.fetchWithProducts(collectionId).then((collection) => {
+    if (shopify.url.searchParams.get("collection")) {
+        const collectionId = shopify.getCollectionId(); //36392834523
+        shopify.store.collection.fetchWithProducts(collectionId).then((collection) => {
             printProducts(collection.products);
         });
         // show all products
         //
     } else {
-        client.product.fetchAll().then((products) => {
+        shopify.store.product.fetchAll().then((products) => {
             printProducts(products);
         });
     }
